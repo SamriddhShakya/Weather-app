9f5c63445994a89aa50899432e132e07e09414a4
SamriddhShakya <117750413+SamriddhShakya@users.noreply.github.com>
2023-12-22 08:31:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7fc7b9439dmsh8e68aa3c20fa9cep1f8a74jsn94388ad317dd',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+const getWeather = (city)=>{
+    cityName.innerHTML=city
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city,options)
+    .then(response=>response.json())
+    .then(response=>{
+
+        console.log(response)
+        
+        cloud_pct.innerHTML = response.cloud_pct
+        //cloud_pct2.innerHTML = response.cloud_pct
+        temp.innerHTML = response.temp
+        temp2.innerHTML = response.temp
+        feels_like.innerHTML = response.feels_like
+        humidity.innerHTML = response.humidity
+        humidity2.innerHTML = response.humidity
+        min_temp.innerHTML = response.min_temp
+        max_temp.innerHTML = response.max_temp
+        wind_speed.innerHTML = response.wind_speed
+        //wind_speed2.innerHTML = response.wind_speed
+        wind_degrees.innerHTML = response.wind_degrees
+        sunrise.innerHTML = response.sunrise
+        sunset.innerHTML = response.sunset
+    })
+    .catch(err=>console.error(err));
+}
+
+submit.addEventListener("click",(e)=>{
+    e.preventDefault()
+    getWeather(city.value)
+})
+getWeather("Delhi")
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seoul',options)
+    .then(response=>response.json())
+    .then(response=>{
+        console.log(response)
+        Seoulcloud_pct.innerHTML = response.cloud_pct
+        Seoultemp.innerHTML=response.temp
+        Seoulfeels_like.innerHTML = response.feels_like
+        Seoulhumidity.innerHTML = response.humidity
+        Seoulmin_temp.innerHTML = response.min_temp
+        Seoulmax_temp.innerHTML = response.max_temp
+        Seoulwind_speed.innerHTML = response.wind_speed
+        //wind_speed2.innerHTML = response.wind_speed
+        Seoulwind_degrees.innerHTML = response.wind_degrees
+        Seoulsunrise.innerHTML = response.sunrise
+        Seoulsunset.innerHTML = response.sunset
+    })
+    .catch(err=>console.error(err));
